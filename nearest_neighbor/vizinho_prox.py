756395886b4513eabf3a7bce49550616ba3e748b
Guilherme <guilherme.leo2012@gmail.com>
2024-08-26 12:50:23 -0300

    Ajustes feitos na verif de tipo e criação da chamada para funções e criação base da função euclidiana

--- nearest_neighbor/vizinho_prox.py
+++ nearest_neighbor/vizinho_prox.py
@@ -1,4 +1,4 @@
-arquivo = open("mapa.txt", encoding="utf-8")
+arquivo = open("PISI2-flyfood/nearest_neighbor/mapa.txt", encoding="utf-8")
 linha = arquivo.readline().split()
 # Listando as palavras chaves que indicam inicio de fornecimento de dados
 keywords = [
@@ -17,21 +17,52 @@ while linha[0] not in keywords:
     if linha[0] == "EDGE_WEIGHT_TYPE:":
         tipo_de_pontos = linha[1]
     elif linha[0] == "EDGE_WEIGHT_FORMAT":
-        tipo_de_pontos = linha[1]
+        pass
     elif linha[0] == "EDGE_DATA_FORMAT":
-        tipo_de_pontos = linha[1]
+        pass
     elif linha[0] == "NODE_COORD_TYPE:":
-        tipo_de_pontos = linha[1]
+        pass
     elif linha[0] == "DISPLAY_DATA_TYPE:":
-        tipo_de_pontos = linha[1]
+        pass
     linha = arquivo.readline().split()
 
+# Guardando todas as linhas de dados em formato de tupla(por enquanto)
 pontos = []
 linha = arquivo.readline().split()
 while linha[0] != "EOF":
     pontos += [tuple(linha)]
     linha = arquivo.readline().split()
 
+
+# Definindo as funções que calcula as distâncias
+def euclidean_distance(tipo, nodes):
+    """Faz o cálculo da distancia euclidiana, seja 2D ou 3D"""
+    visitados = [nodes[0]]
+    nao_visitados = [nodes[1:]]
+    return visitados, nao_visitados
+    # if tipo == "EUC_2":
+    #     pass
+    # else:
+    #     pass
+
+
+# Verificar e chamar a função de calculo da distância correspondente ao tipo
+if tipo_de_pontos in ("EUC_2D", "EUC_3"):
+    saida = euclidean_distance(tipo_de_pontos, pontos)
+elif tipo_de_pontos in ("MAN_2D", "MAN_3"):
+    pass
+elif tipo_de_pontos in ("MAX_2D", "MAX_3"):
+    pass
+elif tipo_de_pontos == "GEO":
+    pass
+elif tipo_de_pontos == "ATT":
+    pass
+elif tipo_de_pontos == "CEIL_2D":
+    pass
+elif tipo_de_pontos in ("XRAY2", "XRAY3"):
+    pass
+
+print(saida)
 
 print(tipo_de_pontos)
 print(pontos[0][1])
