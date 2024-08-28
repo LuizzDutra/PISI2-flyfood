dbdfa9d6396a2171e9f1237e9ce861bdd2f9709c
LuizzDutra <luizzidutra@gmail.com>
2024-08-28 16:28:09 -0300

    Melhoria do código genético

--- genetico/algo_genetico.py
+++ genetico/algo_genetico.py
@@ -1,9 +1,9 @@
 from typing import List, Dict
 from random import randint, uniform
-import tsplib95
 
-POPULATION_SIZE = 100
-MAX_GEN = 100
+POPULATION_SIZE = 1000
+MAX_GEN = 200
+MUTATION_CHANCE = 0.07
 
 
 def make_random_path(nodes: Dict[int, List[int]]) -> List[int]:
@@ -17,12 +17,34 @@ def make_random_path(nodes: Dict[int, List[int]]) -> List[int]:
 
 
 def mutate(vetor: List[int]) -> List[int]:
-    i: int = randint(0, len(vetor)-2)
-    j: int = randint(i+1, len(vetor))
+    mutation_size = randint(2, len(vetor)-1)
+    i: int = randint(0, len(vetor)-mutation_size)
+    j: int = i+mutation_size
 
     split = vetor[i:j+1]
     split.reverse()
     return vetor[:i] + split + vetor[j+1:]
+
+def crossover(s: List[int], t: List[int]) -> List[List[int]]:
+    offspring = [s[:], t[:]]
+    crossover_size = randint(1, len(s))
+    s_point = randint(0, len(s)-crossover_size)
+    child = offspring[0]
+    for i in range(s_point, s_point+crossover_size):
+        for j in range(0, len(child)):
+            if child[j] == t[i]:
+                temp = child[i]
+                child[i] = child[j]
+                child[j] = temp
+    child = offspring[1]
+    for i in range(s_point, s_point+crossover_size):
+        for j in range(0, len(child)):
+            if child[j] == s[i]:
+                temp = child[i]
+                child[i] = child[j]
+                child[j] = temp
+    return offspring
+
 
 def get_dis(a: List[float], b: List[float], nodes: Dict[int, List[int]]):
     x = nodes[a]
@@ -43,42 +65,59 @@ def get_chosen(population_fit: List[int]) -> int:
         search_sum += fit/fit_sum
         if point <= search_sum:
             return i
-    return -1
 
 
-def main():
-    problem = tsplib95.load('berlin52.tsp')
-    nodes: Dict = problem.as_keyword_dict()['NODE_COORD_SECTION']
 
+def start(nodes: Dict[int, List[int]]):
     #Cria população
     population: List[List[int]] = [int]*POPULATION_SIZE
     for i in range(POPULATION_SIZE):
         population[i] = make_random_path(nodes)
 
     #Gerações
-    population_fit = [int]*len(population)
+    population_fit = [int]*POPULATION_SIZE
     new_population = [int]*POPULATION_SIZE
-    for _ in range(MAX_GEN):
-        for i, lone in enumerate(population):
-            population_fit[i] = get_fit(lone, nodes)
 
-        for i in range(POPULATION_SIZE):
-            chosen_idx = get_chosen(population_fit)
-            new_population[i] = mutate(population[chosen_idx])
+    #fitness initialization
+    for i, lone in enumerate(population):
+        population_fit[i] = get_fit(lone, nodes)
+
+    for _ in range(MAX_GEN):
+         #crossover
+        for i in range(0, POPULATION_SIZE, 2):
+            
+            chosen_idx = (get_chosen(population_fit), get_chosen(population_fit))
+            offspring = crossover(population[chosen_idx[0]], population[chosen_idx[1]])
+            
+            #offspring mutation chance
+            if uniform(0, 100) <= MUTATION_CHANCE:
+                offspring[0] = mutate(offspring[0])
+            if uniform(0, 100) <= MUTATION_CHANCE:
+                offspring[1] = mutate(offspring[1])
+            
+            offspring_fit = get_fit(offspring[0], nodes), get_fit(offspring[1], nodes)
+
+
+            for j in range(2):
+                if offspring_fit[j] > population_fit[i+j]:
+                    new_population[i+j] = offspring[j]
+                    population_fit[i+j] = offspring_fit[j]
+                else:
+                    new_population[i+j] = population[chosen_idx[j]]
+                    population_fit[i+j] = population_fit[chosen_idx[j]]
+
         population = new_population
     
     #Resultado final
+    end_idx = -1
+    end_fit = 0
     for i, lone in enumerate(population):
-            population_fit[i] = get_fit(lone, nodes)
-    print(sorted(population_fit)[0]**(-1))
+        if population_fit[i] > end_fit:
+            end_fit = population_fit[i]
+            end_idx = i
 
-
-
-
-
-
-
+    return population[end_idx], 1/end_fit
 
 
 if __name__ == '__main__':
-    main()
+    print(start({1: [5, 4], 2:[3, 2], 3:[10, 7]}))
