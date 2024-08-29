b527ac544b146041c4e0cc4b10e9003837281ac8
LuizzDutra <luizzidutra@gmail.com>
2024-08-29 16:36:53 -0300

    Otimização do algoritmo genético

--- genetico/algo_genetico.py
+++ genetico/algo_genetico.py
@@ -1,9 +1,10 @@
 from typing import List, Dict
-from random import randint, uniform
+from random import randint, uniform, choices
+from math import dist
 
 POPULATION_SIZE = 1000
 MAX_GEN = 200
-MUTATION_CHANCE = 0.07
+MUTATION_CHANCE = 1
 
 
 def make_random_path(nodes: Dict[int, List[int]]) -> List[int]:
@@ -29,27 +30,23 @@ def crossover(s: List[int], t: List[int]) -> List[List[int]]:
     offspring = [s[:], t[:]]
     crossover_size = randint(1, len(s))
     s_point = randint(0, len(s)-crossover_size)
-    child = offspring[0]
     for i in range(s_point, s_point+crossover_size):
-        for j in range(0, len(child)):
-            if child[j] == t[i]:
-                temp = child[i]
-                child[i] = child[j]
-                child[j] = temp
-    child = offspring[1]
-    for i in range(s_point, s_point+crossover_size):
-        for j in range(0, len(child)):
-            if child[j] == s[i]:
-                temp = child[i]
-                child[i] = child[j]
-                child[j] = temp
+        for j in range(0, len(offspring[0])):
+            if offspring[0][j] == t[i]:
+                offspring[0][i], offspring[0][j] = offspring[0][j], offspring[0][i]
+                break
+        for j in range(0, len(offspring[0])):
+            if offspring[1][j] == s[i]:
+                offspring[1][i], offspring[1][j] = offspring[1][j], offspring[1][i]
+                break
+            
     return offspring
 
 
 def get_dis(a: List[float], b: List[float], nodes: Dict[int, List[int]]):
     x = nodes[a]
     y = nodes[b]
-    return ((x[0] - y[0])**2 + (x[1] - y[1])**2)**(1/2)
+    return dist(x, y)
 
 def get_fit(vetor: List[int], nodes: Dict[int, List[int]]) -> int:
     fitness = get_dis(vetor[0], vetor[-1], nodes)
@@ -57,26 +54,17 @@ def get_fit(vetor: List[int], nodes: Dict[int, List[int]]) -> int:
         fitness += get_dis(vetor[i], vetor[i+1], nodes)
     return 1/fitness
 
-def get_chosen(population_fit: List[int]) -> int:
-    fit_sum = sum(population_fit)
-    point = uniform(0, 1)
-    search_sum = 0
-    for i, fit in enumerate(population_fit):
-        search_sum += fit/fit_sum
-        if point <= search_sum:
-            return i
-
-
 
 def start(nodes: Dict[int, List[int]]):
     #Cria população
-    population: List[List[int]] = [int]*POPULATION_SIZE
+    population: List[List[int]] = [List[int]]*POPULATION_SIZE
     for i in range(POPULATION_SIZE):
         population[i] = make_random_path(nodes)
 
     #Gerações
     population_fit = [int]*POPULATION_SIZE
     new_population = [int]*POPULATION_SIZE
+    new_population_fit = [int]*POPULATION_SIZE
 
     #fitness initialization
     for i, lone in enumerate(population):
@@ -85,8 +73,7 @@ def start(nodes: Dict[int, List[int]]):
     for _ in range(MAX_GEN):
          #crossover
         for i in range(0, POPULATION_SIZE, 2):
-            
-            chosen_idx = (get_chosen(population_fit), get_chosen(population_fit))
+            chosen_idx = choices(range(len(population_fit)), weights=population_fit, k=2)
             offspring = crossover(population[chosen_idx[0]], population[chosen_idx[1]])
             
             #offspring mutation chance
@@ -101,13 +88,18 @@ def start(nodes: Dict[int, List[int]]):
             for j in range(2):
                 if offspring_fit[j] > population_fit[i+j]:
                     new_population[i+j] = offspring[j]
-                    population_fit[i+j] = offspring_fit[j]
+                    new_population_fit[i+j] = offspring_fit[j]
                 else:
                     new_population[i+j] = population[chosen_idx[j]]
-                    population_fit[i+j] = population_fit[chosen_idx[j]]
+                    new_population_fit[i+j] = population_fit[chosen_idx[j]]
 
         population = new_population
-    
+        population_fit = new_population_fit
+        print(_)
+        print(1/max(population_fit))
+        print()
+
+
     #Resultado final
     end_idx = -1
     end_fit = 0
@@ -120,4 +112,5 @@ def start(nodes: Dict[int, List[int]]):
 
 
 if __name__ == '__main__':
+
     print(start({1: [5, 4], 2:[3, 2], 3:[10, 7]}))
